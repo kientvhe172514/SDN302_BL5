cd94fff58ea92237a2d6638dfdada0d750302054
Nguyễn Thái Sơn <sonnthe172793@fpt.edu.vn>
2025-08-24 01:33:06 +0700

    update API about subject

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userRouter = require('../routes/user.routes')
+const userRouter = require('../routes/user.routes');
+const subjectRouter = require('../routes/subject.routes');
 
-router.use('/user',userRouter);
+router.use('/user', userRouter);
+router.use('/subjects', subjectRouter);
 
 module.exports = router;
--- server/routes/subject.routes.js
+++ server/routes/subject.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SubjectController = require('../controllers/SubjectController');
 const verifyToken = require('../middleware/verifyToken');
-const authorization = require('../middleware/authorization');
+const { checkAuth } = require('../middleware/authorization');
 
 const subjectController = new SubjectController();
 
@@ -12,13 +12,13 @@ router.get('/credits', subjectController.getSubjectsByCredits);
 router.use(verifyToken);
 
 // Admin only routes
-router.post('/', authorization('admin'), subjectController.createSubject);
-router.put('/:id', authorization('admin'), subjectController.updateSubject);
-router.delete('/:id', authorization('admin'), subjectController.deleteSubject);
+router.post('/', checkAuth('admin'), subjectController.createSubject);
+router.put('/:id', checkAuth('admin'), subjectController.updateSubject);
+router.delete('/:id', checkAuth('admin'), subjectController.deleteSubject);
 
 // Admin and teacher routes
-router.get('/', authorization('admin', 'teacher'), subjectController.getAllSubjects);
-router.get('/:id', authorization('admin', 'teacher'), subjectController.getSubjectById);
-router.get('/code/:subjectCode', authorization('admin', 'teacher'), subjectController.getSubjectByCode);
+router.get('/', checkAuth('admin', 'teacher'), subjectController.getAllSubjects);
+router.get('/:id', checkAuth('admin', 'teacher'), subjectController.getSubjectById);
+router.get('/code/:subjectCode', checkAuth('admin', 'teacher'), subjectController.getSubjectByCode);
 
 module.exports = router;
