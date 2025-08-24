bb3ed37b39948168aa24814ae51b4aa5259c19b0
duongtran1411 <200813455+duongtd1411@users.noreply.github.com>
2025-08-24 17:04:41 +0700

    duong get user add user page , api add class, get class

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userRouter = require('../routes/user.routes')
-
+const classRouter = require('../routes/class.routes')
 router.use('/user',userRouter);
-
+router.use('/class',classRouter)
 module.exports = router;
